8b78186884f6050f24dbc33e8dc4f14047c70f47
Qaisarah <sitiqaisarah.mahidir@gmail.com>
2025-10-31 11:37:52 +0800

    Add files via upload

---
+++ ComplaintManagementSystem/Models/Complaint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ComplaintManagementSystem.Models
+{
+    public class Complaints
+    {
+        public int ComplaintID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
